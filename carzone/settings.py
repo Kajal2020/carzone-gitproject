a59d1d34f1de9940a712438424e0d1f8ff3517ca
kajal2020 <kajalpoojari555@gmail.com>
2020-08-18 10:05:09 -0700

    update

--- carzone/settings.py
+++ carzone/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'cars.apps.CarsConfig',
     'pages.apps.PagesConfig',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -76,8 +77,11 @@ WSGI_APPLICATION = 'carzone.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME':'carzone_db',
+        'USER':'postgres',
+        'PASSWORD':'Mango@2020',
+        'HOST':'localhost',
     }
 }
 
@@ -123,3 +127,6 @@ STATIC_ROOT=os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS=[
         os.path.join(BASE_DIR,'carzone/static'),
 ]
+#media settings
+MEDIA_ROOT=os.path.join(BASE_DIR,'media')
+MEDIA_URL='/media/'
